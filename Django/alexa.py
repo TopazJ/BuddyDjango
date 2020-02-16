39794d443bc3e1100beaf6454428b9c831b7dfb9
R Lee <alpha.r.lee.dev@gmail.com>
2020-02-15 22:03:58 -0700

    Added alexa app

--- Django/alexa.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.http import JsonResponse
-
-
-def index(request):
-    return HttpResponse("<h1>Hello world!</h1><p>You look like you're searching for the API endpoint</p>")
